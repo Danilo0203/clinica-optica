e0891397cf29196c08818787f8e0e38dcc692c61
Danilo0203 <danilocalderon0203@gmail.com>
2025-06-05 22:15:31 -0600

    Agrega un nuevo layout para la autenticación y actualiza las páginas de inicio de sesión y registro para utilizar formularios simplificados. Se eliminan las referencias a componentes obsoletos y se mejora la estructura del código.

--- src/app/auth/sign-up/[[...sign-up]]/page.tsx
+++ src/app/auth/sign-up/[[...sign-up]]/page.tsx
@@ -1,28 +1,11 @@
-import { Metadata } from 'next';
-import SignUpViewPage from '@/features/auth/components/sign-up-view';
+import { RegisterForm } from "@/features/auth/views/register-form";
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'Authentication | Sign In',
-  description: 'Sign In page for authentication.'
+  title: "Registro",
+  description: "Página de registro",
 };
 
 export default async function Page() {
-  let stars = 3000; // Default value
-
-  try {
-    const response = await fetch(
-      'https://api.github.com/repos/kiranism/next-shadcn-dashboard-starter',
-      {
-        next: { revalidate: 86400 }
-      }
-    );
-
-    if (response.ok) {
-      const data = await response.json();
-      stars = data.stargazers_count || stars; // Update stars if API response is valid
-    }
-  } catch (error) {
-    // Error fetching GitHub stars, using default value
-  }
-  return <SignUpViewPage stars={stars} />;
+  return <RegisterForm />;
 }
